11fa00c244e49e2711803e2597e86eaf4dc9a4a2
alexxouou <jore.alex17@gmail.com>
2026-05-04 14:23:42 +0200

    feat: Update image URLs to Google Drive links
    Replaces local image file references with direct Google Drive share links to ensure consistent image loading and accessibility across different environments.

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -6,25 +6,25 @@
 
 export const IMAGE_IDS = {
   // Hero Section
-  HERO_BG: "https://drive.google.com/file/d/17VhWNVEnxA7fjPVwduxM-FK44U6ngGqT/view?usp=drive_link", // Replace with ID if on Drive
+  HERO_BG: "https://drive.google.com/file/d/17VhWNVEnxA7fjPVwduxM-FK44U6ngGqT/view?usp=drive_link.jpg", // Replace with ID if on Drive
 
   // Dresses Collection
-  DRESS_AUDREY: "82255b362eaf01f4bc4747ca1309fd49.jpg",
-  DRESS_GRACE: "Vivienne Westwood.jpg",
-  DRESS_MARILYN: "c4379ef34ef3f5a87a64dd61d04b58e4.jpg",
+  DRESS_AUDREY: "https://drive.google.com/file/d/1AN8fhANIfsTzFNF79534FID8vbL5r8SE/view?usp=drive_linkjpg",
+  DRESS_GRACE: "https://drive.google.com/file/d/1q3oabhz6Y0Ak2Obm2bWNGIJX1NLtfEgk/view?usp=drive_link.jpg",
+  DRESS_MARILYN: "https://drive.google.com/file/d/1AN8fhANIfsTzFNF79534FID8vbL5r8SE/view?usp=drive_link.jpg",
 
   // Shoes Collection
-  SHOE_CINDERELLA: "7b907c823619aa1ecb525b70aa991cdc.jpg",
-  SHOE_HOLLYWOOD: "084cfdf071d5fb617e3ff702f6cfd89f.jpg",
+  SHOE_CINDERELLA: "https://drive.google.com/file/d/1rXxePhrKbmUQD6_ZzE_U-Pfc3c0TKSAh/view?usp=drive_link.jpg",
+  SHOE_HOLLYWOOD: "https://drive.google.com/file/d/1tLa69v0kgTm7Bqk1hGCBuJ--tNyFNegw/view?usp=drive_link.jpg",
 
   // About Section
-  ABOUT_MAIN: "This model wore a trio of dresses for her lemon yellow, Italian summer-inspired wedding.jpg",
-  ABOUT_DETAIL: "82255b362eaf01f4bc4747ca1309fd49.jpg",
+  ABOUT_MAIN: "https://drive.google.com/file/d/1uMtxSHSJSCQj2DLKSa2yQlVzjMrCXiL2/view?usp=drive_link.jpg",
+  ABOUT_DETAIL: "https://drive.google.com/file/d/1AN8fhANIfsTzFNF79534FID8vbL5r8SE/view?usp=drive_link.jpg",
 
   // Blog Posts
-  BLOG_DRAPERY: "Vivienne Westwood.jpg",
-  BLOG_CINEMA: "This model wore a trio of dresses for her lemon yellow, Italian summer-inspired wedding.jpg",
-  BLOG_TRENDS: "c4379ef34ef3f5a87a64dd61d04b58e4.jpg",
+  BLOG_DRAPERY: "https://drive.google.com/file/d/1q3oabhz6Y0Ak2Obm2bWNGIJX1NLtfEgk/view?usp=drive_link.jpg",
+  BLOG_CINEMA: "https://drive.google.com/file/d/1uMtxSHSJSCQj2DLKSa2yQlVzjMrCXiL2/view?usp=drive_link.jpg",
+  BLOG_TRENDS: "https://drive.google.com/file/d/1q-mKbDpHnRnNkrSvAUqhnp154H9U8yQL/view?usp=drive_link.jpg",
 };
 
 export const GOOGLE_DRIVE_BASE_URL = "https://lh3.googleusercontent.com/d/";
