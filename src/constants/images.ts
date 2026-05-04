ea6ba34ac121d2c5c9b136bd48a83f380d0cba62
alexxouou <jore.alex17@gmail.com>
2026-05-04 14:46:43 +0200

    fix: Use Google Drive file IDs directly
    Updated image constants to use raw Google Drive file IDs instead of full URLs. This simplifies image referencing and ensures correct loading from Google Drive.

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -6,25 +6,25 @@
 
 export const IMAGE_IDS = {
   // Hero Section
-  HERO_BG: "https://drive.google.com/file/d/17VhWNVEnxA7fjPVwduxM-FK44U6ngGqT/view?usp=drive_link.jpg", // Replace with ID if on Drive
+  HERO_BG: "17VhWNVEnxA7fjPVwduxM-FK44U6ngGqT",
 
   // Dresses Collection
-  DRESS_AUDREY: "https://drive.google.com/file/d/1AN8fhANIfsTzFNF79534FID8vbL5r8SE/view?usp=drive_linkjpg",
-  DRESS_GRACE: "https://drive.google.com/file/d/1q3oabhz6Y0Ak2Obm2bWNGIJX1NLtfEgk/view?usp=drive_link.jpg",
-  DRESS_MARILYN: "https://drive.google.com/file/d/1AN8fhANIfsTzFNF79534FID8vbL5r8SE/view?usp=drive_link.jpg",
+  DRESS_AUDREY: "1AN8fhANIfsTzFNF79534FID8vbL5r8SE",
+  DRESS_GRACE: "1q3oabhz6Y0Ak2Obm2bWNGIJX1NLtfEgk",
+  DRESS_MARILYN: "1AN8fhANIfsTzFNF79534FID8vbL5r8SE",
 
   // Shoes Collection
-  SHOE_CINDERELLA: "https://drive.google.com/file/d/1rXxePhrKbmUQD6_ZzE_U-Pfc3c0TKSAh/view?usp=drive_link.jpg",
-  SHOE_HOLLYWOOD: "https://drive.google.com/file/d/1tLa69v0kgTm7Bqk1hGCBuJ--tNyFNegw/view?usp=drive_link.jpg",
+  SHOE_CINDERELLA: "1rXxePhrKbmUQD6_ZzE_U-Pfc3c0TKSAh",
+  SHOE_HOLLYWOOD: "1tLa69v0kgTm7Bqk1hGCBuJ--tNyFNegw",
 
   // About Section
-  ABOUT_MAIN: "https://drive.google.com/file/d/1uMtxSHSJSCQj2DLKSa2yQlVzjMrCXiL2/view?usp=drive_link.jpg",
-  ABOUT_DETAIL: "https://drive.google.com/file/d/1AN8fhANIfsTzFNF79534FID8vbL5r8SE/view?usp=drive_link.jpg",
+  ABOUT_MAIN: "1uMtxSHSJSCQj2DLKSa2yQlVzjMrCXiL2",
+  ABOUT_DETAIL: "1AN8fhANIfsTzFNF79534FID8vbL5r8SE",
 
   // Blog Posts
-  BLOG_DRAPERY: "https://drive.google.com/file/d/1q3oabhz6Y0Ak2Obm2bWNGIJX1NLtfEgk/view?usp=drive_link.jpg",
-  BLOG_CINEMA: "https://drive.google.com/file/d/1uMtxSHSJSCQj2DLKSa2yQlVzjMrCXiL2/view?usp=drive_link.jpg",
-  BLOG_TRENDS: "https://drive.google.com/file/d/1q-mKbDpHnRnNkrSvAUqhnp154H9U8yQL/view?usp=drive_link.jpg",
+  BLOG_DRAPERY: "1q3oabhz6Y0Ak2Obm2bWNGIJX1NLtfEgk",
+  BLOG_CINEMA: "1uMtxSHSJSCQj2DLKSa2yQlVzjMrCXiL2",
+  BLOG_TRENDS: "1q-mKbDpHnRnNkrSvAUqhnp154H9U8yQL",
 };
 
 export const GOOGLE_DRIVE_BASE_URL = "https://lh3.googleusercontent.com/d/";
