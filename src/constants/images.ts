c13b2440286cde2d1f9ec72bcdefa7c265944c3b
alexxouou <jore.alex17@gmail.com>
2026-05-05 23:32:05 +0200

    feat: Update image IDs for Maison Étoile
    Replaces outdated Google Drive file IDs with correct ones across hero, dresses, shoes, about, and blog sections. This ensures assets display properly.

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -6,12 +6,12 @@
 
 export const IMAGE_IDS = {
   // Hero Section
-  HERO_BG: "https://drive.google.com/file/d/17VhWNVEnxA7fjPVwduxM-FK44U6ngGqT/view?usp=drive_link",
+  HERO_BG: "17VhWNVEnxA7fjPVwduxM-FK44U6ngGqT",
 
   // Dresses Collection
   DRESS_AUDREY: "1AN8fhANIfsTzFNF79534FID8vbL5r8SE",
   DRESS_GRACE: "1q3oabhz6Y0Ak2Obm2bWNGIJX1NLtfEgk",
-  DRESS_MARILYN: "1AN8fhANIfsTzFNF79534FID8vbL5r8SE",
+  DRESS_MARILYN: "1XC1YsN62uu9AE0TFL75G1LEbqJ-9w3D5",
 
   // Shoes Collection
   SHOE_CINDERELLA: "1rXxePhrKbmUQD6_ZzE_U-Pfc3c0TKSAh",
@@ -19,10 +19,10 @@ export const IMAGE_IDS = {
 
   // About Section
   ABOUT_MAIN: "1uMtxSHSJSCQj2DLKSa2yQlVzjMrCXiL2",
-  ABOUT_DETAIL: "1AN8fhANIfsTzFNF79534FID8vbL5r8SE",
+  ABOUT_DETAIL: "1R3ofRcrrZ7Pi_EYbRlDi5abxFnJ25F92",
 
   // Blog Posts
-  BLOG_DRAPERY: "1q3oabhz6Y0Ak2Obm2bWNGIJX1NLtfEgk",
+  BLOG_DRAPERY: "1HH7vMI7BdLZ5fLzN8xeAG6PF4MQ4xYI-",
   BLOG_CINEMA: "1uMtxSHSJSCQj2DLKSa2yQlVzjMrCXiL2",
   BLOG_TRENDS: "1q-mKbDpHnRnNkrSvAUqhnp154H9U8yQL",
 };
